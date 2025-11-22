8f68180a1603597daa63c514fdeafab271ef3a5c
Igor Lima <igor.pflima@gmail.com>
2025-11-22 09:27:51 -0300

    Init commit

---
+++ src/br/edu/icev/ead/forense/MinhaAnaliseForense.java
@@ -0,0 +1,14 @@
+package br.edu.icev.ead.forense;
+import br.edu.icev.aed.forense.AnaliseForenseAvancada;
+import br.edu.icev.aed.forense.Alerta;
+
+import java.util.*;
+import java.io.*;
+
+
+public class MinhaAnaliseForense implements AnaliseForenseAvancada {
+    @Override
+    public Set<String> encontrarSessoesInvalidas(String arquivo) throws IOException {
+        // Implementar usando Map<String, Stack<String>>
+    }
+}
